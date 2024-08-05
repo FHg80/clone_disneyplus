816d88ca1478228e60ec1de5b6c45a0514db80e4
Pedro de Barros <pedro.de.barros.alves@gmail.com>
2024-08-05 10:53:47 -0300

    header

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+        if (posicaoAtual < alturaHero) {
+            hideHeaderElements();
+        } else {
+            showHeaderElements();
+        }
+    })
+
+
+    // Seção de atrações, programação das abas
     for (let i = 0; i<buttons.length; i++) {
         buttons[i].addEventListener('click', function(e) {
             const abaAlvo = e.target.dataset.tabButton;
@@ -13,11 +27,14 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // Seção FAQ, accordion
     for (let i = 0; i<questions.length; i++) {
         questions[i].addEventListener('click',openOrCloseAnswer);
     }
 })
 
+
+// Declaração de funções
 function hideAllTabs() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
@@ -39,4 +56,14 @@ function openOrCloseAnswer(e) {
     const elementoPai = e.target.parentNode;
 
     elementoPai.classList.toggle(classe);
+}
+
+function hideHeaderElements() {
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
 }
